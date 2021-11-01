9dfd92149fda3063a1f7a82ca7eb3ad47dc181c7
Sven-27 <sven.notermans@live.nl>
2021-11-01 14:16:28 +0100

    added header

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import Details from "components/pages/Details";
 import Home from "components/pages/Home";
+import Header from "components/layout/Header";
 
 function App() {
   return (
     <Router>
       <div className="app">
+        <Header />
         <Switch>
           <Route path="/details">
             <Details />
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+  return (
+    <div>
+      <h1>I am the header component</h1>
+    </div>
+  )
+}
+
+export default Header
