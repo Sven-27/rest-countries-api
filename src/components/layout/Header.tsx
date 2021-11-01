602c6a78a6bbb1ee87260f93e05c564d3c20ffc2
Sven-27 <sven.notermans@live.nl>
2021-11-01 14:43:43 +0100

    added react icons & made header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,10 +1,15 @@
-
+import "styles/layout/Header.scss";
+import { FaRegMoon } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <div>
-      <h1>I am the header component</h1>
-    </div>
+    <header className="header">
+      <h1>Where in the world?</h1>
+      <div className="header__theme">
+        <FaRegMoon />
+        <p>Dark Mode</p>
+      </div>
+    </header>
   )
 }
 
