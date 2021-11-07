820521e065839ce655edbc908bd86886c5cab34d
Sven-27 <sven.notermans@live.nl>
2021-11-07 15:25:32 +0100

    set up the custom select component

--- src/components/pages/home/CustomSelect.tsx
+++ src/components/pages/home/CustomSelect.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react'
-import "styles/pages/home/CustomSelect.scss"
+import { useState } from "react";
+import "styles/pages/home/CustomSelect.scss";
+import { Link } from "react-router-dom";
 
 const CustomSelect = () => {
   return (
@@ -9,11 +10,11 @@ const CustomSelect = () => {
       </div>
       <div className="dropdown-container">
         <ul className="dropdown-list">
-          <li><a href="#">Europe</a></li>
-          <li><a href="#">America</a></li>
-          <li><a href="#">Africa</a></li>
-          <li><a href="#">Asia</a></li>
-          <li><a href="#">Oceania</a></li>
+          <li><Link to="/" className="list-item">Europe</Link></li>
+          <li><Link to="/" className="list-item">America</Link></li>
+          <li><Link to="/" className="list-item">Africa</Link></li>
+          <li><Link to="/" className="list-item">Asia</Link></li>
+          <li><Link to="/" className="list-item">Oceania</Link></li>
         </ul>
       </div>
       
